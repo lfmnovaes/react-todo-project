28676e5c5158455b68c2b80a31e449d98fb0eaa9
Luis <lfmnovaes@gmail.com>
2021-10-25 14:48:42 -0300

    Fix last commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function TodoItem(props) {
-  return (
-    <li>
-      <input type="checkbox" /> {this.props.todo.title}
-    </li>
-  )
-};
+  return <li>{props.todo.title}</li>
+}
 
 export default TodoItem;
