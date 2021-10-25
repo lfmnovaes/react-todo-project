bb71c08079f33b035d603bce18e05fa974cee4f0
Luis <lfmnovaes@gmail.com>
2021-10-25 16:37:57 -0300

    Add style - part 3

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,28 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
 
 function TodoItem(props) {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  }
+
+  const { completed, id, title } = props.todo;
+
   return (
-    <li>
+    <li className={styles.item}>
       <input
         type="checkbox"
-        checked={props.todo.completed}
-        onChange={() => props.handleChangeProps(props.todo.id)}
+        className={styles.checkbox}
+        checked={completed}
+        onChange={() => props.handleChangeProps(id)}
       />
-      {props.todo.title}
-      <button onClick={() => props.deleteTodoProps(props.todo.id)}>
-        Delete
-      </button>
+      <button onClick={() => props.deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
     </li>
-  )
+  );
 }
 
 export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import TodoContainer from "./components/TodoContainer";
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
